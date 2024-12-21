16d2aa6f9ff586174636788905cc344600ae0298
saronmeles4 <saronmeles4@gmail.com>
2024-12-22 01:38:50 +0300

    set up project environment

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import MenuItemCard from "../components/MenuItemCard/MenuItemCard";
+import CategorySection from "../components/CategorySection/CategorySection";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <MenuItemCard />
+      <CategorySection />
+    </>
+  );
+};
+
+export default Home;
